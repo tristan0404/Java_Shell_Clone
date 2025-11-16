135787d8fe5986ab6a30c129b2b5a6d99c62b9a3
tristan0404 <tristanreddy2603@gmail.com>
2025-11-16 17:18:50 +0200

    Implemented single quote functionality

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,50 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Main {
+    private static List<String> parseCommand(String command) {
+        List<String> elements = new ArrayList<>();
+        StringBuilder input = new StringBuilder();
+        boolean quotes = false;
+
+        for (int i = 0; i < command.length(); i++) {
+            char c = command.charAt(i);
+            if (c == '\'') {
+                quotes = !quotes;
+            } else if (c == ' ' && !quotes) {
+                if (input.length() > 0) {
+                    elements.add(input.toString());
+                    input = new StringBuilder();
+                }
+            }
+            else{
+                input.append(c);
+            }
+        }
+        if (input.length() > 0) {
+            elements.add(input.toString());
+        }
+        return elements;
+    }
     public static void main(String[] args) throws Exception {
 
         String currDir = System.getProperty("user.dir");
         while(true) {
             System.out.print("$ ");
             Scanner sc = new Scanner(System.in);
-            String[] command = sc.nextLine().split(" ");
+            List<String> list = parseCommand(sc.nextLine());
+            String[] command = list.toArray(new String[0]);
 
             if (String.join(" ", command).equals("exit 0")) {
                 System.exit(0);
             } else if (command[0].equals("echo")) {
-                String echo = String.join(" ", command).substring(5);
-                System.out.println(echo);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 1; i < command.length; i++) {
+                    if (i > 1) {
+                        builder.append(' ');
+                    }
+                    builder.append(command[i]);
+                }
+                System.out.println(builder);
             }
             else if (command[0].equals("type")) {
                 if (command[1].equals("echo") || command[1].equals("type")|| command[1].equals("exit") || command[1].equals("pwd")) {
